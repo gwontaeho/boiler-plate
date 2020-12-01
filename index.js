c9279f0dfb883c1b80ac0691e9ec9080d09ada5c
gwontaeho <taeho8199@gmail.com>
2020-12-01 22:23:43 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://taeho:xogh1234@boilerplate.phhwb.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -13,6 +20,21 @@ mongoose.connect('mongodb+srv://taeho:xogh1234@boilerplate.phhwb.mongodb.net/<db
 .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello!'))
+
+
+app.post('/register', (req, res) => {
+    //회원 가입 시 필요한 정보들을 client에서 가져오면
+    //정보를 db에 저장
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
